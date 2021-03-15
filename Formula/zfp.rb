bcac139f6788e7a54db65e87188be450a946eefa
ajm-asiaa <ajm@asiaa.sinica.edu.tw>
2021-03-15 09:52:17 +0800

    removing verison line

--- Formula/zfp.rb
+++ Formula/zfp.rb
@@ -2,7 +2,6 @@ class Zfp < Formula
   desc "This is zfp needed by carta_backend"
   homepage "https://github.com/LLNL/zfp"
   url "https://github.com/LLNL/zfp/releases/download/0.5.5/zfp-0.5.5.tar.gz"
-  version "0.5.5"
   sha256 "ce5440334b3eac2e058724faa4c6e4478ca1d81ea087e55ccca33f1996752aad"
   license "MIT"
 
